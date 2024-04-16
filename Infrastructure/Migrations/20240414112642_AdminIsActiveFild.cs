7c4e88ebc10403c73e1f26aaebc39a594b5fb7e0
nucleus accumbens <bezlissa@gmail.com>
2024-04-16 12:11:24 +0700

    newMigrationHistory

---
+++ Infrastructure/Migrations/20240414112642_AdminIsActiveFild.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminIsActiveFild : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Admins",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Admins");
+        }
+    }
+}
--- Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9661b95a9908f0e43336e61ed7d9f567b575bd9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7f69fd5437577d952b4b48def04a615528c179")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
